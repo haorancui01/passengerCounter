20b61aad5dbdf927867f419dc14556b42519870d
haorancui01 <haorancui2001@gmail.com>
2023-06-06 22:41:37 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let count = 0;
+
+//document.getElementById("count-el").innerText = 5;
+
+count = count + 1;
+console.log(count);
+
+//initialize the count as 0
+//listen for clicks on the increment button
+//change the count-el in the HTML to reflect the new count
+
+increment = () => {
+    console.log("The btn is clicked");
+}
+
+function myLogger(){
+    console.log(42);
+}
+
+myLogger();
